81a3ace63dc7206d9c1a00c8f9f497dfc7a53d31
Dylan Roberts <dtroberts@knights.ucf.edu>
2021-12-23 09:56:40 -0500

    1-setup-ui-for-bid-list: Improved UI for the bid list items.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -22,6 +22,8 @@ export class LoginComponent implements OnInit {
   }
 
   login(){
+    this.loginTxt = 'sarah@sarah.com';
+    this.passwordTxt = 'sarah1234';
     this.authService.SignIn(this.loginTxt, this.passwordTxt)
       .then(() => {
         this.router.navigate(['/dashboard']);
