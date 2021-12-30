bda2f0ec076c3e850a1770dea9b7673e0a7f0887
Dylan Roberts <dtroberts@knights.ucf.edu>
2021-12-30 11:25:10 -0500

    5-Updated messages UI to display actual message previews.

--- src/app/messaging/messaging.component.ts
+++ src/app/messaging/messaging.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { User } from '../models/user';
+import { AuthService } from '../services/auth.service';
+import { MessageService } from '../services/message.service';
 import { UserService } from '../services/user.service';
+
+type MessagePrev = {user: User, contents: string, dateStr: string};
 
 @Component({
   selector: 'app-messaging',
@@ -11,16 +15,36 @@ export class MessagingComponent implements OnInit {
   convoListExpanded: boolean = false;
   @Output() createNewMsgtCallback : EventEmitter<any> = new EventEmitter();
   availUsers!: User[];
+  previews: MessagePrev[] = [];
 
   constructor(
     private userService: UserService,
+    private messageService: MessageService,
+    private authService: AuthService,
   ) { }
 
   ngOnInit(): void {
     this.userService.getAll()
       .subscribe(
-        res => {
-          console.log({"users":res});
+        users => {
+          console.log({"users":users});
+          // Get The most recent message for each user that the logged-in user has contacted
+          if (Array.isArray(users)){
+            users.forEach((user) => {
+              this.messageService.getMessagesBetweenUsers(<string>this.authService.getAccount()?.key, <string>user.key)
+                .subscribe((messages) => {
+                  if (Array.isArray(messages) && messages.length){
+                    let message = messages[messages.length - 1];
+                    this.previews.push(
+                      {
+                        user: user, 
+                        contents: message.msgText ? message.msgText : '',
+                        dateStr: message.msgDateStrFormatted ? message.msgDateStrFormatted : '',
+                      });
+                  }
+                })
+            })
+          }
         }
 
       );
